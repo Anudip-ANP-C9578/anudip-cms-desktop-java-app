5324f179b30ec98f20c8608d3f8cbc82c0a8516c
Nitin Kumar <nitinbbk2@gmail.com>
2024-12-19 01:07:09 +0530

    Employee Dao

---
+++ src/main/java/org/anudip/App.java
@@ -0,0 +1,7 @@
+package org.anudip;
+
+public class App {
+	public static void main(String[] args) {
+		new MainFrame(); // creating instance of main frame
+	}
+}
---
+++ src/main/java/org/anudip/EmployeeDashboardFrame.java
@@ -0,0 +1,25 @@
+package org.anudip;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class EmployeeDashboardFrame extends JFrame {
+	JLabel lblWelcome;
+    
+	// constructor
+	public EmployeeDashboardFrame(String employeeName) {
+		setTitle("Employee Dashboard");
+		setSize(400, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+
+		lblWelcome = new JLabel("Welcome to the Employee Dashboard, " + employeeName);
+
+		panel.add(lblWelcome);
+		
+		getContentPane().add(panel);
+		setVisible(true);
+	}
+}
